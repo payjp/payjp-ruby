d6f35848fca98ca8e1d5b4d7890ac0233a07108f
danny <danny.dev8@gmail.com>
2017-03-27 21:55:31 +0900

    Support request header update api

--- lib/payjp/api_operations/update.rb
+++ lib/payjp/api_operations/update.rb
@@ -1,13 +1,13 @@
 module Payjp
   module APIOperations
     module Update
-      def save(params = {})
+      def save(params = {}, opts = {})
         values = self.class.serialize_params(self).merge(params)
 
         if values.length > 0
           values.delete(:id)
 
-          response, opts = request(:post, url, values)
+          response, opts = request(:post, url, values, opts)
           refresh_from(response, opts)
         end
         self
