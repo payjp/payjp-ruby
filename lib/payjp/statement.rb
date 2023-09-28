e1d41e8cec0db857096b6fc1a1de9835678d482f
nonz250 <nozomi.hosaka@binc.jp>
2023-09-28 12:59:24 +0900

    update: Statement api エンドポイントの URL 変更

--- lib/payjp/statement.rb
+++ lib/payjp/statement.rb
@@ -2,15 +2,15 @@ module Payjp
   class Statement < APIResource
     include Payjp::APIOperations::List
 
-    def create_download_urls(params = {}, opts = {})
-      response, opts = request(:post, create_download_urls_url, params, opts)
+    def create_statement_urls(params = {}, opts = {})
+      response, opts = request(:post, create_statement_urls_url, params, opts)
       response
     end
 
     private
 
-    def create_download_urls_url
-      url + '/download_urls'
+    def create_statement_urls_url
+      url + '/statement_urls'
     end
   end
 end
