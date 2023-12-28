939521dfab41ff916eb3050f65040d002e19525d
Daiki Arai <darai0512@yahoo.co.jp>
2023-12-28 17:33:19 +0900

    rename statement_urls POST API useful method name

--- lib/payjp/statement.rb
+++ lib/payjp/statement.rb
@@ -2,14 +2,14 @@ module Payjp
   class Statement < APIResource
     include Payjp::APIOperations::List
 
-    def create_statement_urls(params = {}, opts = {})
-      response, opts = request(:post, create_statement_urls_url, params, opts)
+    def statement_urls(params = {}, opts = {})
+      response, opts = request(:post, statement_urls_url, params, opts)
       response
     end
 
     private
 
-    def create_statement_urls_url
+    def statement_urls_url
       url + '/statement_urls'
     end
   end
