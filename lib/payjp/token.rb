a87e6c326e690d9cf456d9829f6dca6dc0a8bf46
nyamanaka <natsuki-yamanaka@binc.jp>
2024-11-12 17:23:52 +0900

    add tds_finish

--- lib/payjp/token.rb
+++ lib/payjp/token.rb
@@ -1,5 +1,19 @@
 module Payjp
   class Token < APIResource
     include Payjp::APIOperations::Create
+    
+    def self.tds_finish(id, params = {}, opts = {})
+      response, opts = request(:post, url + '/' + id + '/tds_finish', params, opts)
+      Util.convert_to_payjp_object(response, opts)
+    end
+
+    def tds_finish(params = {}, opts = {})
+      response, opts = request(:post, tds_finish_url, params, opts)
+      refresh_from(response, opts)
+    end
+
+    def tds_finish_url
+      url + '/tds_finish'
+    end
   end
 end
